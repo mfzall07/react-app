6ae7c2276d5579dcaa60ab62edcaa3044216a942
mfzall07 <mfaisalhafid@gmail.com>
2022-04-02 13:33:45 +0700

    React JS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Header from "./components/Header";
 
 function App() {
+
+  const [product, setProduct] = useState ([
+    { id: 1, title: 'Product 1', price: 889 },
+    { id: 2, title: 'Product 2', price: 890 },
+    { id: 3, title: 'Product 3', price: 789 },
+    { id: 4, title: 'Product 4', price: 589 },
+    { id: 5, title: 'Product 5', price: 877 }
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      <ul>
+        { product.map((product) => (
+          <li key={product.id}>{product.title} - {product.price}</li>
+        )) }
+      </ul>
     </div>
   );
 }
